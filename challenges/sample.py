01d54c685e0d9c5ca6a97a4b6963f073bb6ac412
Carlos Diez <cdiezmoran@gmail.com>
2017-02-07 14:50:47 -0800

    better solution

--- challenges/sample.py
+++ challenges/sample.py
@@ -7,19 +7,16 @@ import file_reader
 def get_sample_from(histogram):
     sorted_histogram = sorted(histogram.items(), key=operator.itemgetter(1))
 
-    distribution_dict = {}
+    total_sum = sum([tup[1] for tup in sorted_histogram])
     frequency_sum = 0
+
+    random_number = random.randint(1, total_sum)
+
     for tup in sorted_histogram:
-        key = tup[1] + frequency_sum
-        distribution_dict[key] = tup[0]
         frequency_sum += tup[1]
-
-    random_number = random.randint(1, frequency_sum)
-
-    while not random_number in distribution_dict:
-        random_number += 1;
-
-    return distribution_dict[random_number]
+        if random_number <= frequency_sum:
+            print "The frequency is %d\nThe random number is %d" % (frequency_sum, random_number)
+            return tup[0]
 
 if __name__ == '__main__':
     file_path = sys.argv[1]
